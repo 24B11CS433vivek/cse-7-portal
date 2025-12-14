b8642c8828708605d2044fe8caca0abf04b69e73
Vivek <vivek@example.com>
2025-12-15 00:22:43 +0530

    Use Railway PORT environment variable

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const crypto = require('crypto');
 const mime = require('mime-types');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // --- CONFIGURATION ---
 const DATA_DIR = path.join(__dirname, 'materials');
